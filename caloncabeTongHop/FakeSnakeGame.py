fecbaf3af1d8910e708ff5f8755448f2e5fa6abb
Phu Dinh <you@>
2021-05-26 21:49:37 +0700

    cap nhap backup server khi qua tai + giao dien don gian hon

--- caloncabeTongHop/FakeSnakeGame.py
+++ caloncabeTongHop/FakeSnakeGame.py
@@ -5,8 +5,9 @@ import pygame
 from pygame.locals import *
 from textInput import TextInputBox
 
+
 class Snake:
-    def __init__(self, parent_screen,background_image, length,color):
+    def __init__(self, parent_screen, background_image, length, color):
         self.imageDuoi = pygame.image.load("tainguyen/duoi.png").convert()
         self.parent_screen = parent_screen
         self.image = pygame.image.load("tainguyen/player.png").convert()
@@ -15,9 +16,9 @@ class Snake:
         self.background_image = background_image
 
         self.length = length
-        self.x = [self.size]*length
-        self.y = [self.size]*length
-        self.toaDo =[]
+        self.x = [self.size] * length
+        self.y = [self.size] * length
+        self.toaDo = []
         self.color = color
 
     def move_left(self):
@@ -40,9 +41,9 @@ class Snake:
 
     def walk(self):
         # update body
-        for i in range(self.length-1,0,-1):
-            self.x[i] = self.x[i-1]
-            self.y[i] = self.y[i-1]
+        for i in range(self.length - 1, 0, -1):
+            self.x[i] = self.x[i - 1]
+            self.y[i] = self.y[i - 1]
 
         # update head
         if self.direction == 'left':
@@ -83,38 +84,39 @@ class Strawberry:
         try:
             self.parent_screen.blit(self.image, (self.x, self.y))
         except Exception as e:
-            print("chua co du lieu dau tay",e, "du lieu day tay ->",self.x,self.y)
+            print("chua co du lieu dau tay", e, "du lieu day tay ->", self.x, self.y)
 
-    def move(self,x,y):
+    def move(self, x, y):
         self.x = x
         self.y = y
 
 
 class AnotherSnake:
-    def __init__(self,parent_screen, parent_screen_image, x, y):
+    def __init__(self, parent_screen, parent_screen_image, x, y):
         self.parent_screen = parent_screen
         self.parent_screen_image = parent_screen_image
         self.image = pygame.image.load("tainguyen/player2.png").convert()
         self.x = x
         self.y = y
-        self.rect = (x, y, 26,26)
+        self.rect = (x, y, 26, 26)
         self.size = 26
         self.mau_vitri_another = []
 
     def draw(self):
         if self.mau_vitri_another is not None:
-            print(len(self.mau_vitri_another),"so luong nguoi choi")
-            for indexmau,mauVaToaDo in enumerate(self.mau_vitri_another):
+            print(len(self.mau_vitri_another), "so luong nguoi choi")
+            for indexmau, mauVaToaDo in enumerate(self.mau_vitri_another):
                 try:
-                    for index,toaDo in enumerate(mauVaToaDo[1]):
+                    for index, toaDo in enumerate(mauVaToaDo[1]):
                         if index == 0:
-                            self.parent_screen_image.blit(self.image, (toaDo[0],toaDo[1]))
+                            self.parent_screen_image.blit(self.image, (toaDo[0], toaDo[1]))
                             continue
-                        pygame.draw.rect(self.parent_screen_image, mauVaToaDo[0], pygame.Rect(toaDo[0], toaDo[1], 26, 26))
+                        pygame.draw.rect(self.parent_screen_image, mauVaToaDo[0],
+                                         pygame.Rect(toaDo[0], toaDo[1], 26, 26))
                 except Exception as e:
-                    print("None " ,e,indexmau)
+                    print("None ", e, indexmau)
 
-    def set_X_and_Y(self,mausac_va_toado_moi):
+    def set_X_and_Y(self, mausac_va_toado_moi):
         self.mau_vitri_another = mausac_va_toado_moi
 
 
@@ -123,12 +125,13 @@ class Network:
         self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.server = "139.162.49.190"
         self.port = 65432
-        self.addr = (self.server, self.port)
         # self.client.get
-        self.p = self.connect()
         self.mineIP = ""
 
     def getPlayerInfor(self):
+        print("toi ne ",self.port)
+        self.addr = (self.server, self.port)
+        self.p = self.connect()
         return self.p
 
     def connect(self):
@@ -145,20 +148,13 @@ class Network:
             data = pickle.loads(self.client.recv(2048))
             return data
             # tuong ung voi dautayXY (mau sac, vi tri)
-        except pickle.UnpicklingError as fuck:
-            print("het slot")
-            # newgame = Game()
-            # newgame.network.port += 1
-            # print(newgame.network.port)
-            # newgame.run()
-            # return None
-
         except socket.error as e:
             print(e)
             print("soc")
         except EOFError as f:
             print(f)
             print("Connection Closed")
+
 
 class Game:
     def __init__(self):
@@ -170,16 +166,16 @@ class Game:
         self.Height = 694
         self.Weight = 1015
         self.running = True
-        self.surface = pygame.display.set_mode((self.Weight,self.Height))
-        self.surfaceSecond = pygame.display.set_mode((self.Weight,self.Height))
+        self.surface = pygame.display.set_mode((self.Weight, self.Height))
+        self.surfaceSecond = pygame.display.set_mode((self.Weight, self.Height))
         self.background_image = pygame.image.load("tainguyen/seaBG.png").convert()
-        self.surface.blit(self.background_image,(0,0))
-        self.surface.blit(self.surfaceSecond,(0,0))
+        self.surface.blit(self.background_image, (0, 0))
+        self.surface.blit(self.surfaceSecond, (0, 0))
         # self.surfaceSecond.blit(self.background_image,(0,0))
-        self.snake = Snake(self.surface,self.surfaceSecond, 2,("#5404CA"))
+        self.snake = Snake(self.surface, self.surfaceSecond, 2, ("#5404CA"))
         self.snake.draw()
-        self.anotherSnake = AnotherSnake(parent_screen=self.surface,parent_screen_image=self.surfaceSecond,
-                                         x=20,y=20)
+        self.anotherSnake = AnotherSnake(parent_screen=self.surface, parent_screen_image=self.surfaceSecond,
+                                         x=20, y=20)
         self.anotherSnake.draw()
         self.strawberry = Strawberry(self.surface)
         self.strawberry.draw()
@@ -192,46 +188,37 @@ class Game:
         self.eatSound = pygame.mixer.Sound("tainguyen/eat.wav")
         self.zoGameSound = pygame.mixer.Sound("tainguyen/heheboiz.wav")
 
-
-    # def is_collision(self, x1, y1, x2, y2):
-    #     if x2 <= x1 < x2 + self.size:
-    #         if y2 <= y1 < y2 + self.size:
-    #             self.level += 10
-    #             print(self.level)
-    #             return True
-    #     return False
-
     def is_ban_muoi(self, x1, y1):
         if x1 < 0 or y1 < 0 or x1 > self.Weight or y1 > self.Height:
             return True
         else:
             return False
 
-    def display_die(self,num):
+    def display_die(self, num):
         self.display_score()
-        font = pygame.font.SysFont('arial',80)
+        font = pygame.font.SysFont('arial', 80)
         if num % 2 == 1:
-            score2 = font.render(f" Try Again With Space ",True,(238,147,86))
-            self.surface.blit(score2,(120,420))
+            score2 = font.render(f" Try Again With Space ", True, (238, 147, 86))
+            self.surface.blit(score2, (120, 420))
 
-        font = pygame.font.SysFont('arial',300)
-        die = font.render("you die",True,(107,58,73))
-        self.surface.blit(die,(10,20))
-        self.surface.blit(self.surfaceSecond,(0,0))
+        font = pygame.font.SysFont('arial', 300)
+        die = font.render("you die", True, (107, 58, 73))
+        self.surface.blit(die, (10, 20))
+        self.surface.blit(self.surfaceSecond, (0, 0))
 
     def display_score(self):
-        font = pygame.font.SysFont('arial',18,bold=True)
-        score = font.render(f"{self.playerName} : {self.snake.length}",True,("#7259E3"))
-        self.surface.blit(score,(770,5))
+        font = pygame.font.SysFont('arial', 18, bold=True)
+        score = font.render(f"{self.playerName} : {self.snake.length}", True, ("#CEFAD9"))
+        self.surface.blit(score, (770, 5))
 
     def display_tutorial(self):
-        font = pygame.font.SysFont('arial',30)
-        score = font.render(f"  Type your name and Enter !",True,(255,255,255))
-        self.surface.blit(score,(300,250))
+        font = pygame.font.SysFont('arial', 30)
+        score = font.render(f"  Type your name and Enter !", True, (255, 255, 255))
+        self.surface.blit(score, (300, 250))
 
     def play(self):
-        self.surface.blit(self.background_image,(0,0))
-        self.surface.blit(self.surfaceSecond,(0,0))
+        self.surface.blit(self.background_image, (0, 0))
+        self.surface.blit(self.surfaceSecond, (0, 0))
         self.anotherSnake.draw()
         self.snake.walk()
         self.strawberry.draw()
@@ -279,59 +266,69 @@ class Game:
         self.playerName, self.snake.color = self.network.send(self.playerName)
 
     def run(self):
+        hetSlot = False
         print("run")
         self.playerName = self.getPlayerName()
         if self.playerName == "JustQuitPlease":
             return
+
         print(self.playerName)
         self.zoGameSound.play()
         self.gui_va_dinh_danh()
         while self.running:
-
-            self.gui_va_phan_tach_du_lieu()
+            try:
+                self.gui_va_phan_tach_du_lieu()
+            except pickle.UnpicklingError as fuck:
+                print("het slot", fuck)
+                hetSlot = True
+                break
             for event in pygame.event.get():
                 if event.type == KEYDOWN:
                     if event.key == K_ESCAPE:
                         self.running = False
-
                     if event.key == K_LEFT:
                         self.snake.move_left()
-
                     if event.key == K_SPACE:
                         self.snake.dash()
-
                     if event.key == K_RIGHT:
                         self.snake.move_right()
-
                     if event.key == K_UP:
                         self.snake.move_up()
-
                     if event.key == K_DOWN:
                         self.snake.move_down()
-
                 elif event.type == QUIT:
                     self.running = False
 
             self.play()
+            print(self.level)
             self.clock.tick(self.level)
 
-        self.network.send(self.playerName+"ENDGAME"+str(self.snake.length)+"ENDGAME"+str(self.network.mineIP))
-        num = 0
-        while not self.running:
-            num += 1
-            print(num)
-            pygame.display.flip()
-            self.surface.blit(self.background_image, (0, 0))
-            self.display_die(num)
-            for event in pygame.event.get():
-                if event.type == KEYDOWN:
-                    if event.key == K_SPACE:
-                        game = Game()
-                        game.run()
+        if hetSlot:
+            print("out of slot change the port")
+            newgame = Game()
+            newgame.network.port = self.network.port + 1
+            print(newgame.network.port)
+            newgame.run()
+            return
+        else:
+            self.network.send(self.playerName + "ENDGAME" + str(self.snake.length) + "ENDGAME" + str(self.network.mineIP))
+            num = 0
+            while not self.running:
+                num += 1
+                print(num)
+                pygame.display.flip()
+                self.surface.blit(self.background_image, (0, 0))
+                self.display_die(num)
+                for event in pygame.event.get():
+                    if event.type == KEYDOWN:
+                        if event.key == K_SPACE:
+                            game = Game()
+                            game.run()
+                            return
+                    elif event.type == QUIT:
                         return
-                elif event.type == QUIT:
-                    return
-            self.clock.tick(5)
+                self.clock.tick(5)
+
 
     def tui_la_nguoi_may_man(self, ip_tang_diem):
         print(ip_tang_diem)
